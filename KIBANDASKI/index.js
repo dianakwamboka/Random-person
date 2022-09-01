3c055aaf3687b8592fd729e09b8c35dd7402b1d8
Diana Kwamboka <diana.kwamboka@student.moringaschool.com>
2022-09-01 14:30:17 +0300

    kibandaski.html styles.css

---
+++ KIBANDASKI/index.js
@@ -0,0 +1,47 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// (document).ready(function() {
+   
+//     function cafe(africanDishes, asianDishes, drinks, total, yourOrder) {
+//         this.africanDishes = chakula;
+//         this.asianDishes = chakula;
+//         this. drinks= vinywaji;
+//         this.total = total;
+//         this.order = order;
+//       }
+  
+//   $('.order').click(function() {
+   
+//         food =  food(africanDishes, asianDishes, drinks, total, order);
+ 
+//       });
+//       $(".checkout-btn").click(function() {
+//           amountt = amountt + total;
+//           $(".amountt h3 span").html(amountt)
+//          var answer= prompt("Payment method: enter MPESA or Cash")
+//           if (answer =="MPESA") {var location=prompt("forward your mpesacode to this shortcode 25257")
+//           alert("Shukran,enjoy your meal!" + location + "your total amount is " +(amountt ))}
+//      });
+      
+//     });
+    
+
+    
